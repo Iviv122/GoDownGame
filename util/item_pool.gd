33591887a73181d63896f316d7a0c6331639abbb
Iviv122 <ivan.skrinnik.nomia@gmail.com>
2025-08-22 21:54:37 +0200

    Better random

--- util/item_pool.gd
+++ util/item_pool.gd
@@ -5,6 +5,20 @@ class_name ItemPool
 ## change to queue like in modern tetris?
 
 @export var items : Array[Item]
+var queue : Array[Item] = []
+
+func  _ready():
+	reroll()	
+
+func reroll():
+	items.shuffle()
+	for i in items:
+		queue.push_back(i)
 
 func get_item() -> Item:
-	return items.pick_random()
+	var item = queue.back()
+	queue.pop_back()
+	if item == null:
+		reroll()
+		return items.pick_random()
+	return item 
